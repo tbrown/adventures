b5c88733831a655dc3697b24d323623c6b30c737
Tim Brown <tim@tbrown.org>
2024-05-06 09:34:44 -0400

    Try fixing story permalinks again

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
+
+  // Treat files with no extension as HTML
+  eleventyConfig.addPassthroughCopy("*.html"); // Copy HTML files as-is
+  eleventyConfig.addPassthroughCopy("*.css"); // Copy CSS files as-is
+  eleventyConfig.setTemplateFormats(["md", "html", "css"]);
 };
